a125b412c840fa8d604ea0c7d04fc216bcdbe923
pedroedu02 <pedroedu.gc02@gmail.com>
2024-11-23 11:38:48 -0300

    Deploy

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -3,8 +3,13 @@ import mongoose from "mongoose";
 export const connectDatabase = () => {
   let DB_URI = "";
 
+  const DB_USER = process.env.DB_USER;
+  const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+  const DB_CLUSTER = process.env.DB_CLUSTER;
+  const DB_PARAMS = process.env.DB_PARAMS;
+
   if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
-  if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+  if (process.env.NODE_ENV === "PRODUCTION") DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/?${DB_PARAMS}`;
 
   mongoose.connect(DB_URI).then((con) => {
     console.log(
--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -3,10 +3,14 @@ import products from "./data.js";
 import Product from "../models/product.js";
 
 const seedProducts = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
-    );
+    const DB_USER = "leandradevieira38";
+    const DB_PASSWORD = encodeURIComponent("AdvJ85h06va2VcFo");
+    const DB_CLUSTER = "cluster0.jyaqf.mongodb.net";
+    const DB_PARAMS = "retryWrites=true&w=majority&appName=Cluster0";
+  
+    const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/?${DB_PARAMS}`;
+    try {
+      await mongoose.connect(DB_URL);
 
     await Product.deleteMany();
     console.log("Products are deleted");
